4afdc9d579d3964b47b2de7001a17deaf0a2e58c
demo1 <pardeepsingh@orionesolutions.in>
2016-03-11 12:56:02 +0530

    another modification

--- WebApplication1/WebApplication1/WebForm1.aspx.vb
+++ WebApplication1/WebApplication1/WebForm1.aspx.vb
@@ -3,6 +3,8 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Console.WriteLine("Hello !")
+        Console.WriteLine("hdvbdvbd ... ")
+        Console.Write(DateTime.Now())
 
     End Sub
 
