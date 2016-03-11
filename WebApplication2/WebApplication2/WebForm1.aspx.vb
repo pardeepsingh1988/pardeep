cd1a83ab87362116a12c3ca0600cfd51f759be1c
demo1 <pardeepsingh@orionesolutions.in>
2016-03-11 17:39:03 +0530

    done

--- WebApplication2/WebApplication2/WebForm1.aspx.vb
+++ WebApplication2/WebApplication2/WebForm1.aspx.vb
@@ -3,7 +3,7 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Console.WriteLine("hello world !")
-
+        Console.WriteLine("hello world !")
     End Sub
 
 End Class
